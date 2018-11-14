1b909ffca7465fc85f3d44ab691a2a727c39c926
Harry Stevens <hstevens9709@gmail.com>
2018-11-14 22:00:19 +0000

    Added VS solution and project for AI. Added StringIterator.cs which splits an input string at spaces and returns an iterator over the words

---
+++ ai/ai/StringIterator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game_ai
+{
+    public class StringIterator : IEnumerable
+    {
+	    private string[] _inputString;
+
+	    public StringIterator(string inputString)
+	    {
+		    _inputString = inputString.Split(' ');
+	    }
+
+	    IEnumerator IEnumerable.GetEnumerator()
+	    {
+		    return GetEnumerator();
+	    }
+
+	    public IEnumerator GetEnumerator( )
+	    {
+		    foreach (string word in _inputString)
+		    {
+			    yield return word;
+		    }
+	    }
+
+        static void Main()
+        {
+
+        }
+    }
+}
