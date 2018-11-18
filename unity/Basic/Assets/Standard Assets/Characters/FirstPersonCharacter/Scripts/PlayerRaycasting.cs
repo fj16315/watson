4a1399ef8dd65cfc3ce12d8990276b51fd5fd77e
Ashwinder Khurana <ak16625@my.bristol.ac.uk>
2018-11-18 16:13:08 +0000

    NPC interactions, press 'T' to talk popup feature

---
+++ unity/Basic/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/PlayerRaycasting.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerRaycasting : MonoBehaviour {
+
+	public float raycastDistance; 
+	RaycastHit objectHit;
+	bool display = false;
+	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.DrawRay(this.transform.position, this.transform.forward * raycastDistance, Color.yellow);
+
+		if(Physics.Raycast(this.transform.position, this.transform.forward, out objectHit, raycastDistance)){
+			if(Equals(objectHit.collider.gameObject.name, "NPC_Test")){
+				Debug.Log("true");
+				display = true;
+				
+				
+			}
+		}
+		else{
+			display = false;
+		}
+
+	}
+
+	void OnGUI(){
+		if(display == true){
+			Debug.Log("textbox activated");
+			GUI.Box(new Rect((Screen.width-150)/2,(Screen.height-50)/2,150,50 ), "Press T to talk to NPC");
+			
+		}
+	}
+}
