b8c41ef055b9362e3cfff8ddd259b06ecf300f8a
Jago <jtaylor4288@gmail.com>
2018-12-05 20:00:50 +0000

    Added methods, tests, and documentation for Relationships

--- ai/AI/src/Relationships.cs
+++ ai/AI/src/Relationships.cs
@@ -1,35 +1,103 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 
 namespace GameAI
 {
   /// <summary>
-  /// Defines all relationships between two nodes in the knowledge graph.
+  /// Stores the relationships between two nodes in <see cref="GameAI.KnowledgeGraph"/>.
   /// </summary>
   public struct Relationships
   {
     /// <summary>
-    /// Relationship.
+    /// The internal enum used by <see cref="GameAI.Relationships"/>.
     /// </summary>
     /// <remarks>
-    /// Stored in one-hot encoding to allow for or-ing of multiple relationships for efficient storage.
+    /// Stored in one-hot encoding to allow for or-ing of multiple relationships.
     /// Named against standard because of clash with class name.
     /// </remarks>
     [Flags]
-    public enum RelationshipFlags : int
+    public enum Flags : int
     {
       None = 0, Contains = 1,
       Owns = 2, Wants = 4
     };
 
     /// <summary>
-    /// The relationships between two nodes in the knowledge graph, stored as the bitwise or of the Relationships.
+    /// The member of <see cref="GameAI.Relationships"/> that stores the relationships (as <see cref="GameAI.Relationships.Flags"/>).
     /// </summary>
-    public RelationshipFlags relationships
+    public Flags relationships
     {
       get;
       set;
     }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="T:GameAI.Relationships"/> struct.
+    /// </summary>
+    /// <param name="flags">The relationships for this new instance.</param>
+    public Relationships(Flags flags)
+    {
+      relationships = flags;
+    }
+
+    /// <summary>
+    /// Checks if there are no relationships.
+    /// </summary>
+    /// <returns><c>true</c>, if <see cref="GameAI.Relationships.relationships"/> is <see cref="GameAI.Relationships.Flags.None"/>, <c>false</c> otherwise.</returns>
+    public bool IsNone()
+      => this.relationships == Flags.None;
+
+    /// <summary>
+    /// Checks if all of the relationships in <c>that</c> are also present in <c>this</c>.
+    /// </summary>
+    /// <returns><c>true</c> if all of the relationships in <c>that</c> are present in <c>this</c>.</returns>
+    /// <param name="that">The <see cref="GameAI.Relationships"/> which should be contained in <c>this</c>.</param>
+    public bool Contains(Relationships that)
+      => (this & that) == that;
+
+    /// <summary>
+    /// Determines whether a specified instance of <see cref="GameAI.Relationships"/> is equal to another specified <see cref="GameAI.Relationships"/>.
+    /// </summary>
+    /// <param name="lhs">The first <see cref="GameAI.Relationships"/> to compare.</param>
+    /// <param name="rhs">The second <see cref="GameAI.Relationships"/> to compare.</param>
+    /// <returns><c>true</c> if <c>lhs</c> and <c>rhs</c> are equal; otherwise, <c>false</c>.</returns>
+    public static bool operator ==(Relationships lhs, Relationships rhs)
+      => lhs.relationships == rhs.relationships;
+
+    /// <summary>
+    /// Determines whether a specified instance of <see cref="GameAI.Relationships"/> is not equal to another specified <see cref="GameAI.Relationships"/>.
+    /// </summary>
+    /// <param name="lhs">The first <see cref="GameAI.Relationships"/> to compare.</param>
+    /// <param name="rhs">The second <see cref="GameAI.Relationships"/> to compare.</param>
+    /// <returns><c>true</c> if <c>lhs</c> and <c>rhs</c> are not equal; otherwise, <c>false</c>.</returns>
+    public static bool operator !=(Relationships lhs, Relationships rhs)
+      => !(lhs == rhs);
+
+    /// <summary>
+    /// The union (<c>and</c>) of two <see cref="GameAI.Relationships"/>.
+    /// </summary>
+    /// <returns>A new <see cref="GameAI.Relationships"/> containing the union of the relationships in <c>lhs</c> and <c>rhs</c>.</returns>
+    /// <param name="lhs">The left hand side of the expression.</param>
+    /// <param name="rhs">The right hand side of the expression.</param>
+    public static Relationships operator &(Relationships lhs, Relationships rhs)
+      => new Relationships(lhs.relationships & rhs.relationships);
+
+    /// <summary>
+    /// The inclusive disjuction (<c>or</c>) of two <see cref="GameAI.Relationships"/>.
+    /// </summary>
+    /// <returns>A new <see cref="GameAI.Relationships"/> containing the inclusive disjuction of the relationships in <c>lhs</c> and <c>rhs</c>.</returns>
+    /// <param name="lhs">The left hand side of the expression.</param>
+    /// <param name="rhs">The right hand side of the expression.</param>
+    public static Relationships operator |(Relationships lhs, Relationships rhs)
+      => new Relationships(lhs.relationships | rhs.relationships);
+
+    // /// <summary>
+    // /// The exclusive disjuction (<c>xor</c>) of two <see cref="GameAI.Relationships"/>.
+    // /// </summary>
+    // /// <returns>A new <see cref="GameAI.Relationships"/> containing the exclusive disjucntion of the relationships in <c>lhs</c> and <c>rhs</c>.</returns>
+    // /// <param name="lhs">The left hand side of the expression.</param>
+    // /// <param name="rhs">The right hand side of the expression.</param>
+    // public static Relationships operator ^(Relationships lhs, Relationships rhs)
+    //   => new Relationships(lhs.relationships ^ rhs.relationships);
+
   }
 }
--- ai/AITests/RelationshipsTest.cs
+++ ai/AITests/RelationshipsTest.cs
@@ -1,14 +1,156 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GameAI;
+using GameAI.Relationships;
 
 namespace AITests
 {
-    [TestClass]
-    public class RelationshipTests
+  [TestClass]
+  public class RelationshipTests
+  {
+    [TestMethod]
+    public void IsNoneTrue()
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-        }
+      var none = new Relationships();
+      Assert.IsTrue(none.IsNone());
     }
+
+    [TestMethod]
+    public void IsNoneFalse()
+    {
+      var not_none = new Relationships(Flags.Contains);
+      Assert.IsFalse(not_none.IsNone());
+    }
+
+    [TestMethod]
+    public void ContainsTrue()
+    {
+      var larger = new Relationships(Flags.Contains | Flags.Owns);
+      var smaller = new Relationships(Flags.Owns);
+      Assert.IsTrue(larger.Contains(smaller));
+    }
+
+    [TestMethod]
+    public void ContainsFalse()
+    {
+      var larger = new Relationships(Flags.Contains | Flags.Owns);
+      var smaller = new Relationships(Flags.Wants);
+      Assert.IsFalse(larger.Contains(smaller));
+    }
+
+    [TestMethod]
+    public void EqualNone()
+    {
+      var lhs = new Relationships();
+      var rhs = new Relationships(Flags.None);
+      Assert.AreEqual(lhs, rhs);
+    }
+
+    [TestMethod]
+    public void EqualAll()
+    {
+      var lhs = new Relationships(Flags.Contains | Flags.Owns | Flags.Wants);
+      var rhs = new Relationships(Flags.Contains | Flags.Owns | Flags.Wants);
+      Assert.AreEqual(lhs, rhs);
+    }
+
+    [TestMethod]
+    public void NotEqualSingle()
+    {
+      var lhs = new Relationships(Flags.Contains);
+      var rhs = new Relationships(Flags.Wants);
+      Assert.AreNotEqual(lhs, rhs);
+    }
+
+    [TestMethod]
+    public void NotEqualsOverlap()
+    {
+      var lhs = new Relationships(Flags.Owns | Flags.Wants);
+      var lhs = new Relationships(Flags.Contains | Flags.Wants);
+      Assert.AreNotEqual(lhs, rhs);
+    }
+
+    [TestMethod]
+    public void NotEqualContains()
+    {
+      var lhs = new Relationships(Flags.Contains | Flags.Owns | Flags.Wants);
+      var rhs = new Relationships(Flags.Contains | Flags.Owns);
+      Assert.IsTrue(lhs.Contains(rhs));
+      Assert.AreNotEqual(lhs, rhs);
+    }
+
+    [TestMethod]
+    public void NotEqualsAllNone()
+    {
+      var lhs = new Relationships(Flags.Contains | Flags.Owns | Flags.Wants);
+      var rhs = new Relationships();
+      Assert.AreNotEqual(lhs, rhs);
+    }
+
+    [TestMethod]
+    public void AndDisjoint()
+    {
+      var lhs = new Relationships(Flags.Owns | Flags.Wants);
+      var rhs = new Relationships(Flags.Contains);
+      var result = new Relationships(Flags.Contains | Flags.Owns | Flags.Wants);
+      Assert.AreEqual(lhs & rhs, result);
+    }
+
+    [TestMethod]
+    public void AndOverlap()
+    {
+      var lhs = new Relationships(Flags.Owns | Flags.Wants);
+      var rhs = new Relationships(Flags.Contains | Flags.Wants);
+      var result = new Relationships(Flags.Contains | Flags.Owns | Flags.Wants);
+      Assert.AreEqual(lhs & rhs, result);
+    }
+
+    [TestMethod]
+    public void AndNone()
+    {
+      var rel = new Relationships(Flags.Owns | Flags.Wants);
+      var none = new Relationships();
+      Assert.AreEqual(rel & none, none);
+    }
+
+    [TestMethod]
+    public void AndSelf()
+    {
+      var rel = new Relationships(Flags.Wants);
+      Assert.AreEqual(rel & rel, rel);
+    }
+
+    [TestMethod]
+    public void OrDisjoint()
+    {
+      var lhs = new Relationships(Flags.Wants);
+      var rhs = new Relationships(Flags.Owns);
+      var result = new Relationships(Flags.Wants | Flags.Owns);
+      Assert.AreEquals(lhs | rhs, result);
+    }
+
+    [TestMethod]
+    public void OrOverlap()
+    {
+      var lhs = new Relationships(Flags.Wants | Flags.Owns);
+      var rhs = new Relationships(Flags.Wants | Flags.Contains);
+      var result = new Relationships(Flags.Contains | Flags.Owns | Flags.Wants);
+      Assert.AreEquals(lhs | rhs, result);
+    }
+
+    [TestMethod]
+    public void OrNone()
+    {
+      var rel = new Relationships(Flags.Owns);
+      var none = new Relationships();
+      Assert.AreEqual(rel | none, rel);
+    }
+
+    [TestMethod]
+    public void OrSelf()
+    {
+      var rel = new Relationships(Flags.Wants);
+      Assert.AreEqual(rel | rel, rel);
+    }
+  }
 }
