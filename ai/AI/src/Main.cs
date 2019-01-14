5be1bd9606545e1fe36622395c273d66a4bdd3cb
Jago <jtaylor4288@gmail.com>
2019-01-14 22:38:11 +0000

    Integrated the Stanford.NLP.Parser package

--- ai/AI/src/Main.cs
+++ ai/AI/src/Main.cs
@@ -1,11 +1,28 @@
 using GameAI;
+using System;
 
 public class MainClass
 {
   /// <summary>
-  ///   An empty main function for checking that other files compile
+  ///   Runs the parser as a repl environment.
   /// </summary>
   public static void Main(string[] args)
   {
+    var parser = Parser.FromRecommendedPath();
+
+    while (true)
+    {
+      Console.Write("> ");
+      var line = Console.ReadLine();
+      if (line == "") { break; }
+
+      var tree = parser.Parse(line);
+      Console.WriteLine("{0}\n", tree);
+
+      foreach (var td in parser.DependenciesFrom(tree))
+      {
+        Console.WriteLine("{0}", td);
+      }
+    }
   }
 }
---
+++ ai/AI/src/Parser.cs
@@ -0,0 +1,66 @@
+﻿using edu.stanford.nlp.process;
+//using edu.stanford.nlp.ling;
+using edu.stanford.nlp.trees;
+using edu.stanford.nlp.parser.lexparser;
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GameAI
+{
+  public class Parser
+  {
+    private readonly LexicalizedParser _lp;
+    public Parser(string path)
+    {
+      _lp = LexicalizedParser.loadModel(path);
+    }
+
+    public static Parser FromRecommendedPath()
+      => new Parser("../../res/englishPCFG.ser.gz");
+
+    private java.util.List Tokenise(string sentence)
+    {
+      var tokFac = PTBTokenizer.factory(new CoreLabelTokenFactory(), "");
+      var reader = new java.io.StringReader(sentence);
+      var words = tokFac.getTokenizer(reader).tokenize();
+      reader.close();
+
+      return words;
+    }
+
+    public Tree Parse(string sentence)
+      => _lp.apply(Tokenise(sentence));
+
+    public IEnumerable<TypedDependency> DependenciesFrom(Tree tree)
+    {
+      var tdList = new PennTreebankLanguagePack()
+                    .grammaticalStructureFactory()
+                    .newGrammaticalStructure(tree)
+                    .typedDependenciesCCprocessed();
+      return new TypedDependeciesList(tdList);
+    }
+  }
+
+  public class TypedDependeciesList : IEnumerable<TypedDependency>
+  {
+    private java.util.List _tdList;
+
+    public TypedDependeciesList(java.util.List tdList)
+    {
+      _tdList = tdList;
+    }
+
+    public IEnumerator<TypedDependency> GetEnumerator()
+    {
+      var tdIterator = _tdList.iterator();
+      while (tdIterator.hasNext())
+      {
+        yield return tdIterator.next() as TypedDependency;
+      }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+      => GetEnumerator();
+  }
+}
