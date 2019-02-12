888e6eb34e29a17a367993a2702f2954122a4fc3
Unknown <fj16315@my.bristol.ac.uk>
2019-02-12 16:53:35 +0000

    Basic response to questions

---
+++ ai/AI/src/SpeechSynthesis.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameAI
+{
+  public static class SpeechSynthesis
+  {
+    public static string Synthesise(TypedDependenciesList tdList, IEnumerable<Entity> entities, Associations ass)
+    {
+      string sentence = "The " + ass.NameOf(entities.GetEnumerator().Current) + " " + tdList.GetRoot() + " the " + tdList.WithRelationFrom(tdList.GetRoot(), "dobj");
+      return sentence;
+    }
+  }
+}
