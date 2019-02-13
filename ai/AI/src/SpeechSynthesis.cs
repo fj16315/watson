32d9e233dc46589f5583b38d53a5af5d52ca7a44
Unknown <fj16315@my.bristol.ac.uk>
2019-02-13 13:51:54 +0000

    Responds to queries with multiple answers

--- ai/AI/src/SpeechSynthesis.cs
+++ ai/AI/src/SpeechSynthesis.cs
@@ -9,7 +9,36 @@ namespace GameAI
   {
     public static string Synthesise(TypedDependenciesList tdList, IEnumerable<Entity> entities, Associations ass)
     {
-      string sentence = "The " + ass.NameOf(entities.GetEnumerator().Current) + " " + tdList.GetRoot() + " the " + tdList.WithRelationFrom(tdList.GetRoot(), "dobj");
+      string sentence;
+      IEnumerator<Entity> enumerator = entities.GetEnumerator();
+      /*List<Entity> eList = new List<Entity>();
+      eList.Add(entities.GetEnumerator().Current);
+      while(entities.GetEnumerator().MoveNext())
+      {
+        Console.WriteLine("Moving on");
+        eList.Add(entities.GetEnumerator().Current);
+      }*/
+      if (enumerator.Current.Equals(null))
+      {
+        sentence = "I'm sorry I don't know.";
+      }
+      else
+      {
+        //Console.WriteLine(eList.Count());
+        //sentence = "The " + ass.NameOf(eList.ElementAt(0));
+        sentence = "The " + ass.NameOf(enumerator.Current);
+        /*foreach (Entity e in eList) {
+          if (e.Equals(eList.ElementAt(0)) == false)
+          {
+            sentence += " and the " + ass.NameOf(e);
+          }
+        }*/
+        while (enumerator.MoveNext())
+        {
+          sentence += " and the " + ass.NameOf(enumerator.Current);
+        }
+        sentence += " " + tdList.GetRoot().word() + " the " + tdList.WithRelationFrom(tdList.GetRoot(), "dobj").word() + ".";
+      }
       return sentence;
     }
   }
