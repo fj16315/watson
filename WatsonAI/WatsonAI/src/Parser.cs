90993ef04b5aa956cab1a177c09a61a0fd8b53bb
Adam Beddoe <adam.beddoe@gmail.com>
2019-02-26 13:07:26 +0000

    Added NLP library and resources to project, tagdict file not found, for some reason.

--- WatsonAI/WatsonAI/src/Parser.cs
+++ WatsonAI/WatsonAI/src/Parser.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using OpenNLP.Tools.PosTagger;
+using OpenNLP.Tools.Tokenize;
 
 namespace WatsonAI
 {
@@ -9,12 +12,23 @@ namespace WatsonAI
   /// </summary>
   public class Parser
   {
-
     /// <summary>
     /// Initialises a new instance of the <see cref="GameAI.Parser"/> class.
     /// </summary>
     public Parser()
     {
+      var modelPath = Directory.GetCurrentDirectory() + "\\res\\Models\\EnglishPOS.nbin";
+      var tagDictDir = Directory.GetCurrentDirectory() + "\\res\\Models\\Parser\\tagdict";
+
+      var sentence = "The quick brown fox jumped over the lazy dog.";
+
+      var tokenizer = new EnglishRuleBasedTokenizer(false);
+      var tokens = tokenizer.Tokenize(sentence);
+
+      var posTagger = new EnglishMaximumEntropyPosTagger(modelPath, tagDictDir);
+      var pos = posTagger.Tag(tokens);
+      
+      System.Diagnostics.Debug.WriteLine(pos);
     }
   }
 }
