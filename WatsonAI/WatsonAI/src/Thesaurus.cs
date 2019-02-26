635ae5018469c1d619b4029692ce9e0f083ba777
Adam Beddoe <adam.beddoe@gmail.com>
2019-02-26 11:27:37 +0000

    Added comments to Thesaurus.cs file.

--- WatsonAI/WatsonAI/src/Thesaurus.cs
+++ WatsonAI/WatsonAI/src/Thesaurus.cs
@@ -5,20 +5,41 @@ using wordLib = Microsoft.Office.Interop.Word;
 
 namespace WatsonAI
 {
+  /// <summary>
+  /// Class providing synonym checking.
+  /// Uses microsoft word library for checking synonyms. 
+  /// For best results, provide DictionaryWords rather than strings to methods.
+  /// </summary>
   public class Thesaurus
   {
     private wordLib.Application app;
 
+    /// <summary>
+    /// Constructor for a thesaurus.
+    /// Generates new word application so is slow to construct.
+    /// </summary>
     public Thesaurus()
     {
       this.app = new wordLib.Application();
     }
 
+    /// <summary>
+    /// Constructor for a thesaurus.
+    /// Passing in an existing word application is faster for mutliple thesaurus construction.
+    /// </summary>
+    /// <param name="app"></param>
     public Thesaurus(wordLib.Application app)
     {
       this.app = app;
     }
 
+    /// <summary>
+    /// Checks if string a is a synonym of string b.
+    /// Sometimes gives weird results e.g. man is a synonym for cat.
+    /// </summary>
+    /// <param name="a">The first word.</param>
+    /// <param name="b">The second word.</param>
+    /// <returns>A boolean value, whether they are synonyms.</returns>
     public bool IsSynonymOf(string a, string b)
     {
       if (a.Equals(b, StringComparison.OrdinalIgnoreCase)) return true;
@@ -30,6 +51,14 @@ namespace WatsonAI
       return CheckSynonymRelation(b, a);
     }
 
+    /// <summary>
+    /// Checks if string a is a synonym of string b.
+    /// Providing the Lexical Category of the first word helps filter invalid synonyms.
+    /// Sometimes gives weird results e.g. man is a synonym for cat.
+    /// </summary>
+    /// <param name="a">The first word.</param>
+    /// <param name="b">The second word.</param>
+    /// <returns>A boolean value, whether they are synonyms.</returns>
     public bool IsSynonymOf(DictionaryWord a, string b)
     {
       if (a.word.Equals(b, StringComparison.OrdinalIgnoreCase)) return true;
@@ -41,6 +70,14 @@ namespace WatsonAI
       return CheckSynonymRelation(new DictionaryWord(b, a.category), a.word);
     }
 
+    /// <summary>
+    /// Checks if string a is a synonym of string b.
+    /// Providing the Lexical Category of both words helps filter invalid synonyms.
+    /// Sometimes gives weird results e.g. man is a synonym for cat.
+    /// </summary>
+    /// <param name="a">The first word.</param>
+    /// <param name="b">The second word.</param>
+    /// <returns>A boolean value, whether they are synonyms.</returns>
     public bool IsSynonymOf(DictionaryWord a, DictionaryWord b)
     {
       if (a.word.Equals(b.word, StringComparison.OrdinalIgnoreCase)) return true;
@@ -53,6 +90,13 @@ namespace WatsonAI
       return CheckSynonymRelation(b, a.word);
     }
 
+    /// <summary>
+    /// Checks the one way synonym relation for a to b.
+    /// To get all synonyms, this should be called with parameters swapped.
+    /// </summary>
+    /// <param name="a">The first word.</param>
+    /// <param name="b">The second word.</param>
+    /// <returns>Whether b is a synonym for a.</returns>
     private bool CheckSynonymRelation(string a, string b)
     {
       var infosyn = app.SynonymInfo[a, wordLib.WdLanguageID.wdEnglishUK];
@@ -68,6 +112,15 @@ namespace WatsonAI
       return false;
     }
 
+    /// <summary>
+    /// Checks the one way synonym relation for a to b.
+    /// To get all synonyms, this should be called with parameters swapped.
+    /// Uses the lexical category of a to filter synonyms.
+    /// </summary>
+    /// <remarks> The lists are one indexed! </remarks>
+    /// <param name="a">The first word.</param>
+    /// <param name="b">The second word.</param>
+    /// <returns>Whether b is a synonym for a.</returns>
     private bool CheckSynonymRelation(DictionaryWord a, string b)
     {
       var asyn = app.SynonymInfo[a.word, wordLib.WdLanguageID.wdEnglishUK];
@@ -89,11 +142,19 @@ namespace WatsonAI
     }
   }
 
+  /// <summary>
+  /// A struct containing the string for the word and it's lexical category.
+  /// </summary>
   public struct DictionaryWord
   {
     public string word { get; }
     public LexicalCategory category { get; }
 
+    /// <summary>
+    /// A constructor for a DictionaryWord.
+    /// </summary>
+    /// <param name="word">The word.</param>
+    /// <param name="category">The lexical category of the word. (See enum.)</param>
     public DictionaryWord(string word, LexicalCategory category)
     {
       this.word = word;
