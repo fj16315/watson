c8165dbe8219363af903eaee349f6114a8133f3c
Adam Beddoe <adam.beddoe@gmail.com>
2019-03-17 19:05:35 +0000

    Added stemming library and implemented in thesaurus, will need adding to the integration steps.

--- WatsonAI/WatsonAI/src/Thesaurus.cs
+++ WatsonAI/WatsonAI/src/Thesaurus.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Linq;
+using Annytab.Stemmer;
 
 namespace WatsonAI
 {
@@ -16,6 +17,8 @@ namespace WatsonAI
   {
 
     private WordNetEngine wordNet;
+
+    private IStemmer stemmer; 
 
     /// <summary>
     /// Constructor for a thesaurus.
@@ -33,6 +36,8 @@ namespace WatsonAI
       System.Diagnostics.Debug.WriteLine("Loading thesaurus database...");
       wordNet.LoadFromDirectory(directory);
       System.Diagnostics.Debug.WriteLine("Load completed.");
+
+      this.stemmer = new EnglishStemmer();
     }
 
     /// <summary>
@@ -41,9 +46,20 @@ namespace WatsonAI
     /// <remarks>Case sensitive.</remarks>
     /// <param name="first">The first word.</param>
     /// <param name="second">The second word.</param>
+    /// <param name="stemInput">Whether to also try stemming the input.</param>
     /// <returns>True if the first describes the second.</returns>
-    public bool Describes(string first, string second)
-      => wordNet.GetWordSimilarity(first, second) > 0.25;
+    public bool Describes(string first, string second, bool stemInput = false)
+    {
+      bool similar = wordNet.GetWordSimilarity(first, second) > 0.25;
+      if (stemInput)
+      {
+        similar = similar 
+          || wordNet.GetWordSimilarity(stemmer.GetSteamWord(first), second) > 0.25
+          || wordNet.GetWordSimilarity(first, stemmer.GetSteamWord(second)) > 0.25
+          || wordNet.GetWordSimilarity(stemmer.GetSteamWord(first), stemmer.GetSteamWord(second)) > 0.25;
+      }
+      return similar;
+    }
 
     /// <summary>
     /// Checks if the first word describes the second through the built-in WordNet similarity function.
@@ -53,8 +69,22 @@ namespace WatsonAI
     /// <param name="first">The first word.</param>
     /// <param name="second">The second word.</param>
     /// <param name="lexicalCategory">The lexical category of both words.</param>
+    /// <param name="stemInput">Whether to also try stemming the input.</param>
     /// <returns>True if the first describes the second.</returns>
-    public bool Describes(string first, string second, PartOfSpeech lexicalCategory)
+    public bool Describes(string first, string second, PartOfSpeech lexicalCategory, bool stemInput = false)
+    {
+      bool describes = DescribesNoStemming(first, second, lexicalCategory);
+      if (stemInput)
+      { 
+        describes = describes
+          || DescribesNoStemming(stemmer.GetSteamWord(first), second, lexicalCategory)
+          || DescribesNoStemming(first, stemmer.GetSteamWord(second), lexicalCategory)
+          || DescribesNoStemming(stemmer.GetSteamWord(first), stemmer.GetSteamWord(second), lexicalCategory);
+      }
+      return describes;
+    }
+
+    private bool DescribesNoStemming(string first, string second, PartOfSpeech lexicalCategory) 
       => (from synSet in wordNet.GetSynSets(first, lexicalCategory)
           from relation in synSet.SemanticRelations
           from relatedSynSet in synSet.GetRelatedSynSets(relation, true)
@@ -72,28 +102,55 @@ namespace WatsonAI
     /// <param name="second">The second word.</param>
     /// <param name="lexicalCategory">The lexical category of both words.</param>
     /// <param name="relations">The possible relations between the road.</param>
+    /// <param name="stemInput">Whether to also try stemming the input.</param>
     /// <returns>True if the first describes the second.</returns>
-    public bool Describes(string first, string second, PartOfSpeech lexicalCategory, SynSetRelation[] relations)
+    public bool Describes(string first, string second, PartOfSpeech lexicalCategory, 
+      SynSetRelation[] relations, bool stemInput = false)
+    {
+      bool describes = DescribesNoStemming(first, second, lexicalCategory, relations);
+      if (stemInput)
+      { 
+        describes = describes
+          || DescribesNoStemming(stemmer.GetSteamWord(first), second, lexicalCategory, relations)
+          || DescribesNoStemming(first, stemmer.GetSteamWord(second), lexicalCategory, relations)
+          || DescribesNoStemming(stemmer.GetSteamWord(first), stemmer.GetSteamWord(second), lexicalCategory, relations);
+      }
+      return describes;
+    }
+
+    private bool DescribesNoStemming(string first, string second, PartOfSpeech lexicalCategory, SynSetRelation[] relations) 
       => (from synSet in wordNet.GetSynSets(first, lexicalCategory)
           from relation in relations
           from relatedSynSet in synSet.GetRelatedSynSets(relation, true)
           where relatedSynSet.PartOfSpeech == lexicalCategory
           from synonym in synSet.Words.Union(relatedSynSet.Words)
           select synonym.Contains(second))
-        .Any(x => x);
+         .Any(x => x);
 
     /// <summary>
     /// Returns the synonyms of the first word.
     /// </summary>
     /// <remarks>Case sensitive.</remarks>
     /// <param name="word">The word.</param>
+    /// <param name="stemInput">Whether to also try stemming the input.</param>
     /// <returns>The synonyms of the word.</returns>
-    public IEnumerable<string> GetSynonyms(string word)
+    public IEnumerable<string> GetSynonyms(string word, bool stemInput = false)
+    {
+      var synonyms = GetSynonymsNoStemming(word);
+      if (stemInput && !synonyms.Any())
+      {
+        word = stemmer.GetSteamWord(word);
+        synonyms = GetSynonymsNoStemming(word);
+      }
+      return synonyms;
+    }
+
+    private IEnumerable<string> GetSynonymsNoStemming(string word)
       => (from synSet in wordNet.GetSynSets(word)
-         from relation in synSet.SemanticRelations
-         from relatedSynSet in synSet.GetRelatedSynSets(relation, true)
-         from synonym in relatedSynSet.Words.Union(synSet.Words)
-         select synonym).Distinct();
+          from relation in synSet.SemanticRelations
+          from relatedSynSet in synSet.GetRelatedSynSets(relation, true)
+          from synonym in relatedSynSet.Words.Union(synSet.Words)
+          select synonym).Distinct();
 
     /// <summary>
     /// Returns the synonyms of the first word.
@@ -102,14 +159,26 @@ namespace WatsonAI
     /// <remarks>Case sensitive.</remarks>
     /// <param name="word">The word.</param>
     /// <param name="lexicalCategory">The lexical category of the word.</param>
+    /// <param name="stemInput">Whether to also try stemming the input.</param>
     /// <returns>The synonyms of the word.</returns>
-    public IEnumerable<string> GetSynonyms(string word, PartOfSpeech lexicalCategory)
+    public IEnumerable<string> GetSynonyms(string word, PartOfSpeech lexicalCategory, bool stemInput = false)
+    {
+      var synonyms = GetSynonymsNoStemming(word, lexicalCategory);
+      if (stemInput && !synonyms.Any())
+      {
+        word = stemmer.GetSteamWord(word);
+        synonyms = GetSynonymsNoStemming(word, lexicalCategory);
+      }
+      return synonyms;
+    }
+
+    private IEnumerable<string> GetSynonymsNoStemming(string word, PartOfSpeech lexicalCategory)
       => (from synSet in wordNet.GetSynSets(word, lexicalCategory)
-         from relation in synSet.SemanticRelations
-         from relatedSynSet in synSet.GetRelatedSynSets(relation, true)
-         where relatedSynSet.PartOfSpeech == lexicalCategory
-         from synonym in relatedSynSet.Words.Union(synSet.Words)
-         select synonym).Distinct();
+          from relation in synSet.SemanticRelations
+          from relatedSynSet in synSet.GetRelatedSynSets(relation, true)
+          where relatedSynSet.PartOfSpeech == lexicalCategory
+          from synonym in relatedSynSet.Words.Union(synSet.Words)
+          select synonym).Distinct();
 
     /// <summary>
     /// Returns the synonyms of the first word.
@@ -119,14 +188,27 @@ namespace WatsonAI
     /// <param name="word">The word.</param>
     /// <param name="lexicalCategory">The lexical category of the word.</param>
     /// <param name="relations">The possible synSetReltions between synsets accepted.</param>
+    /// <param name="stemInput">Whether to also try stemming the input.</param>
     /// <returns>The synonyms of the word.</returns>
-    public IEnumerable<string> GetSynonyms(string word, PartOfSpeech lexicalCategory, SynSetRelation[] relations)
+    public IEnumerable<string> GetSynonyms(string word, PartOfSpeech lexicalCategory, 
+      SynSetRelation[] relations, bool stemInput = false)
+    {
+      var synonyms = GetSynonymsNoStemming(word, lexicalCategory, relations);
+      if (stemInput && !synonyms.Any())
+      {
+        word = stemmer.GetSteamWord(word);
+        synonyms = GetSynonymsNoStemming(word, lexicalCategory, relations);
+      }
+      return synonyms;
+    }
+
+    private IEnumerable<string> GetSynonymsNoStemming(string word, PartOfSpeech lexicalCategory, SynSetRelation[] relations)
       => (from synSet in wordNet.GetSynSets(word, lexicalCategory)
-         from relation in relations
-         from relatedSynSet in synSet.GetRelatedSynSets(relation, true)
-         where relatedSynSet.PartOfSpeech == lexicalCategory
-         from synonym in relatedSynSet.Words.Union(synSet.Words)
-         select synonym).Distinct();
+          from relation in relations
+          from relatedSynSet in synSet.GetRelatedSynSets(relation, true)
+          where relatedSynSet.PartOfSpeech == lexicalCategory
+          from synonym in relatedSynSet.Words.Union(synSet.Words)
+          select synonym).Distinct();
 
     /// <summary>
     /// Returns the similarity value of two words according to WordNet similarity.
@@ -134,8 +216,26 @@ namespace WatsonAI
     /// <remarks>Case sensitive.</remarks>
     /// <param name="first">The first word.</param>
     /// <param name="second">The second word.</param>
+    /// <param name="stemInput">Whether to also try stemming the input.</param>
     /// <returns>The similarity value of two words.</returns>
-    public float Similarity(string first, string second)
-      => wordNet.GetWordSimilarity(first, second);
+    public float Similarity(string first, string second, bool stemInput = false)
+    {
+      if (stemInput)
+      {
+        first = stemmer.GetSteamWord(first);
+        second = stemmer.GetSteamWord(second);
+      }
+      return wordNet.GetWordSimilarity(first, second);
+    }
+
+    /// <summary>
+    /// Stems a given word. Eg. cats => cat.
+    /// Can sometimes destroy the word if it's already in a stemmed form.
+    /// </summary>
+    /// <param name="word">The word to stem.</param>
+    /// <param name="stemInput">Whether to also try stemming the input.</param>
+    /// <returns>The stemmed word.</returns>
+    public string Stem(string word) 
+      => stemmer.GetSteamWord(word);
   }
 }
