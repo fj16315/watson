567e73539c1acc4da9cb9c4b87e41a0104e19ece
Unknown <fj16315@my.bristol.ac.uk>
2019-03-25 18:01:35 +0000

    Comments for new classes

--- WatsonAI/WatsonAI/src/KnowledgeBuilder.cs
+++ WatsonAI/WatsonAI/src/KnowledgeBuilder.cs
@@ -4,17 +4,28 @@ using System.Text;
 
 namespace WatsonAI
 {
+  /// <summary>
+  /// Builds a new Knowledge object and its Associations
+  /// </summary>
   public class KnowledgeBuilder
   {
     private readonly Knowledge knowledge;
     private readonly Associations associations;
 
+    /// <summary>
+    /// Constructs empty Knowledge and Associations
+    /// </summary>
     public KnowledgeBuilder()
     {
       knowledge = new Knowledge();
       associations = new Associations();
     }
 
+    /// <summary>
+    /// Takes a list of entity names, and generates new Entities for each
+    /// in the Associations
+    /// </summary>
+    /// <param name="entities"></param>
     public void AddEntities(List<string> entities)
     {
       for (int i = 0; i < entities.Count; i++)
@@ -23,6 +34,11 @@ namespace WatsonAI
       }
     }
 
+    /// <summary>
+    /// Takes a list of verb names, and generates new Verbs for each
+    /// in the Associations
+    /// </summary>
+    /// <param name="verbs"></param>
     public void AddVerbs(List<string> verbs)
     {
       for (int i = 0; i < verbs.Count; i++)
@@ -31,6 +47,12 @@ namespace WatsonAI
       }
     }
 
+    /// <summary>
+    /// Adds a new VerbPhrase to Knowledge given a verb and the entities associated with it
+    /// </summary>
+    /// <param name="verb"></param>
+    /// <param name="valents"></param>
+    /// <returns></returns>
     public bool AddPhrase(string verb, List<Tuple<Valent.Tag, string>> valents)
     {
       Verb knowledgeVerb;
@@ -53,6 +75,15 @@ namespace WatsonAI
       return true;
     }
 
+    /// <summary>
+    /// Adds a new VerbPhrase given a simple 3 word statement:
+    /// "subj" -> "verb" -> "dobj"
+    /// e.g. "cat kill earl"
+    /// </summary>
+    /// <param name="subj"></param>
+    /// <param name="verb"></param>
+    /// <param name="dobj"></param>
+    /// <returns></returns>
     public bool AddSimplePhrase(string subj, string verb, string dobj)
     {
       var valents = new List<Tuple<Valent.Tag, string>>();
@@ -61,9 +92,17 @@ namespace WatsonAI
       return AddPhrase(verb, valents);
     }
 
+    /// <summary>
+    /// Gets Associations
+    /// </summary>
+    /// <returns></returns>
     public Associations Associations() 
       => associations;
 
+    /// <summary>
+    /// Gets Knowledge
+    /// </summary>
+    /// <returns></returns>
     public Knowledge Knowledge()
       => knowledge;
   }
--- WatsonAI/WatsonAI/src/Story.cs
+++ WatsonAI/WatsonAI/src/Story.cs
@@ -4,12 +4,18 @@ using System.Text;
 
 namespace WatsonAI
 {
+  /// <summary>
+  /// Creates a Knowledge object and its Associations based on hard-coded story
+  /// </summary>
   public class Story
   {
     private readonly KnowledgeBuilder knowledgeBuilder;
     public Knowledge Knowledge { get; }
     public Associations Associations { get; }
 
+    /// <summary>
+    /// Creates a new Story object and generates the story
+    /// </summary>
     public Story()
     {
       knowledgeBuilder = new KnowledgeBuilder();
@@ -18,6 +24,10 @@ namespace WatsonAI
       this.Associations = knowledgeBuilder.Associations();
     }
 
+    /// <summary>
+    /// Uses hard-coded List<string> for entities and verbs, then adds simple verb phrases
+    /// built from the entities and verbs
+    /// </summary>
     private void GenerateStory()
     {
       var entities = new List<string> { "cat", "earl", "dog" };
