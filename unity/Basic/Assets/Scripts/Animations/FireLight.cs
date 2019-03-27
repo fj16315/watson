2330a98fdc52dc0d3204fd43f234b9f29e9fc9b2
jdvfarrington <james.farrington.2016@my.bristol.ac.uk>
2019-03-27 00:57:56 +0000

    Scene blocked off, lit and set up for tutorial

--- unity/Basic/Assets/Scripts/Animations/FireLight.cs
+++ unity/Basic/Assets/Scripts/Animations/FireLight.cs
@@ -20,19 +20,22 @@ public class FireLight : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+
         
         if ((counter++ % 3 ) == 0)
         {
             distance = Vector3.Distance(transform.position, player.transform.position);
             if (distance < 12)
             {
-                diff = (float)(rand.Next(110, 140));
+                diff = (float)(rand.Next(100, 140));
                 l.intensity = diff / 100.0F;
             }
         }
         if (counter % 8 == 0)
         {
-            if (rand.Next(0, 10) >= 5)
+            distance = Vector3.Distance(transform.position, player.transform.position);
+            if (rand.Next(0, 10) >= 5 && distance < 12)
             {
                 l.transform.Translate(0, hop, 0);
                 counter = 1;
