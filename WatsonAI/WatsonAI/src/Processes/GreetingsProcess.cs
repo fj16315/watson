15261e80137a6f4d1a4b79d2b792a637a613d2a6
Jago <jtaylor4288@gmail.com>
2019-04-01 20:04:38 +0100

    Fixed an indexing issue in GreetingProcess.cs (uobrassmonkey/game#28)

--- WatsonAI/WatsonAI/src/Processes/GreetingsProcess.cs
+++ WatsonAI/WatsonAI/src/Processes/GreetingsProcess.cs
@@ -59,7 +59,7 @@ namespace WatsonAI
         Random rnd = new Random();
         Random watson = new Random();
 
-        var index = rnd.Next(listOfGreetings.Capacity);
+        var index = rnd.Next(listOfGreetings.Count);
         if (watson.Next(2) == 1) 
         { 
           stream.AppendOutput(listOfGreetings[index] + ", Watson" );
