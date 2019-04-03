3c3a7dea74d91287fa5c2347962a463710a1bdde
Joe <Joseph.Mills22@gmail.com>
2019-04-03 15:57:51 +0100

    Added inspection function to Thing script, inspection not finished

--- unity/Basic/Assets/Scripts/Thing.cs
+++ unity/Basic/Assets/Scripts/Thing.cs
@@ -9,8 +9,11 @@ namespace Things
     {
         public NPCController owner;
         public bool pickup;
+        private bool show = false;
         public string objName;
         public int category;
+        public string description;
+        public GUISkin skin;
 
         public enum Category : int {BOOK, KEY, CONTAINER, OBJECT};
 
@@ -23,12 +26,34 @@ namespace Things
         // Update is called once per frame
         void Update()
         {
-
+            
         }
 
         public bool CanPickUp()
         {
             return pickup;
+        }
+
+        //once per GUI update
+        private void OnGUI()
+        {
+            GUI.skin = skin;
+            if (show)
+            {
+                //Draw the GUI layer
+                GUI.Box(new Rect(Screen.width, Screen.height, Screen.width/2, Screen.height/2), description);
+
+                //show until escape pressed
+                if (Event.current.isKey && Event.current.keyCode == KeyCode.Escape)
+                {
+                    show = false;
+                }
+            }
+        }
+
+        public void InspectObject()
+        {
+            show = true;
         }
     }
 }
