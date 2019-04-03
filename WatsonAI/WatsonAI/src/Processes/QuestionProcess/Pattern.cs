c10789ada71b036ecd9b65ab604e261db2659c0d
Jago <jtaylor4288@gmail.com>
2019-04-03 12:16:45 +0100

    Added comments to Pattern.cs

--- WatsonAI/WatsonAI/src/Processes/QuestionProcess/Pattern.cs
+++ WatsonAI/WatsonAI/src/Processes/QuestionProcess/Pattern.cs
@@ -20,25 +20,48 @@ namespace WatsonAI
     public abstract Result<a> Match(Parse tree);
 
     /// <summary>
-    /// The or operator. Uses the Or Pattern to generate the result.
+    /// Returns the <see cref="Result{T}"/> of the first pattern that successfully matches
+    /// with a preference for the left hand side pattern.
     /// </summary>
     /// <param name="lhs">The first pattern.</param>
     /// <param name="rhs">The second pattern.</param>
-    /// <returns>Or of pattern lhs and pattern rhs.</returns>
+    /// <returns>The first successful <see cref="Result{T}"/>.</returns>
     public static Pattern<a> operator |(Pattern<a> lhs, Pattern<a> rhs)
       => new Or<a>(lhs, rhs);
 
+    /// <summary>
+    /// Runs <paramref name="child"/> on the children of
+    /// <paramref name="branch"/>.
+    /// </summary>
+    /// <param name="branch">The parent <see cref="WatsonAI.Branch"/>.</param>
+    /// <param name="child">The child <see cref="WatsonAI.Pattern{T}"/>.</param>
+    /// <returns>The <see cref="Result{T}"/> from <paramref name="child"/>
+    /// if both succeed.</returns>
     public static Children<a> operator >(Branch branch, Pattern<a> child)
       => new Children<a>(branch, child);
 
+    /// <summary>
+    /// Runs <paramref name="descendant"/> on the descendants of
+    /// <paramref name="branch"/>.
+    /// </summary>
+    /// <param name="branch">The ancestor <see cref="WatsonAI.Branch"/>.</param>
+    /// <param name="descendant">The descendant <see cref="WatsonAI.Pattern{T}"/>.</param>
+    /// <returns>The <see cref="Result{T}"/> from <paramref name="descendant"/>
+    /// if both succeed.</returns>
+    public static Descendant<a> operator >=(Branch branch, Pattern<a> descendant)
+      => new Descendant<a>(branch, descendant);
+
+    // This operator is not used, it is only defined as comparison operators
+    // must be defined in pairs. Using this throws a compilation error.
+    [Obsolete("Use operator > for declaring a child relation", true)]
     public static Children<a> operator <(Branch branch, Pattern<a> child)
     {
       throw new NotSupportedException("Operator < is not supported for Patterns, it is just required for compilation.");
     }
 
-    public static Descendant<a> operator >=(Branch branch, Pattern<a> child)
-      => new Descendant<a>(branch, child);
-
+    // This operator is not used, it is only defined as comparison operators
+    // must be defined in pairs. Using this throws a compilation error.
+    [Obsolete("Use operator >= for declaring a descendant relation", true)]
     public static Descendant<a> operator <=(Branch branch, Pattern<a> child)
     {
       throw new NotSupportedException("Operator <= is not supported for Patterns, it is just required for compilation.");
@@ -59,9 +82,25 @@ namespace WatsonAI
 
   public static class PatternExtension
   {
+    /// <summary>
+    /// Flattens one level of the structure inside <paramref name="pattern"/>.
+    /// </summary>
+    /// <returns>A pattern containing the flattened structure.</returns>
+    /// <param name="pattern">Pattern.</param>
+    /// <remarks>Equivalent to <c>fmap join</c>.</remarks>
     public static Pattern<IEnumerable<a>> Flatten<a>(this Pattern<IEnumerable<IEnumerable<a>>> pattern)
       => pattern.Map(e => e.SelectMany(x => x));
 
+    /// <summary>
+    /// Apply <paramref name="func"/> to the <see cref="Result{T}"/> of
+    /// <paramref name="pattern"/>.
+    /// </summary>
+    /// <returns>A <see cref="Pattern{b}"/> with the result of
+    /// <paramref name="func"/>.</returns>
+    /// <param name="pattern">The pattern to match.</param>
+    /// <param name="func">The function to apply.</param>
+    /// <typeparam name="a">The input type.</typeparam>
+    /// <typeparam name="b">The return type.</typeparam>
     public static Pattern<b> Map<a, b>(this Pattern<a> pattern, Func<a, b> func)
       => new Map<a, b>(pattern, func);
   }
