0f124cd5d0479e64ce9cc19f3beb6a98ba6e2c44
Harry Stevens <hs16597@my.bristol.ac.uk>
2019-04-08 15:09:40 +0100

    individual character graphs written (some bits still in comments as with previous commit)

--- WatsonAI/WatsonAI/src/Story_new.cs
+++ WatsonAI/WatsonAI/src/Story_new.cs
@@ -41,7 +41,7 @@ namespace WatsonAI
         {"scrap", "about", "nightshade"},
         {"belongings", "contain", "nightshade"},
         {"colonel", "own", "belongings"},
-        /*{"nightshade", "is", "fast-acting"},*/ 
+        /*{"nightshade", "is", "fast-acting"},*/
         /*{"nightshade", "look like", "blackcurrants"},*/
         /*{"dining room", "contain", "blackcurrants"},*/
         //{"nightshade", "poison", "earl"},
@@ -61,11 +61,12 @@ namespace WatsonAI
         /*{"countess", "wants to sell", "estate"},*/
         {"earl", "use", "barbital"},
         {"book", "about", "barbital"},
+        {"study", "contain", "book"},
         /*{"barbital", "is", "sleeping aid"},*/
         {"bathroom", "contain", "barbital"},
         /*{"earl", "have", "barbital tolerance"},*/
         {"earl", "employ", "butler"},
-        /*{"earl", "stealing from", "earl"},*/
+        /*{"butler", "stealing from", "earl"},*/
         {"butler", "is", "nervous"},
         /*{"butler", "allergic to", "plants"},*/
         /*{"butler", "have", "rat poison"},*/
@@ -74,10 +75,10 @@ namespace WatsonAI
         {"earl", "meet", "gangster"},
         {"gangster", "meet", "earl"},
         /*{"gangster", "kill with", "arsenic"},*/
-        /*{"arsenic", "is", "fast-acting"},*/
+        /*{"arsenic", "is", "slow-acting"},*/
         /*{"arsenic, "used as", "rat poison"},*/
         /*{"master bedroom", "contain", "letter"},*/
-        {"gangster", "send", "letter"}, 
+        {"gangster", "send", "letter"},
         {"earl", "receive", "letter"}
       };
       Associations = universeKnowledgeBuilder.Associations;
@@ -86,31 +87,150 @@ namespace WatsonAI
       Characters = new Dictionary<Names, Character>
       {
         {Names.ACTRESS, new Character("actress", true)},
-        {Names.COUNTESS, new Character("countess", false)},
+        {Names.BUTLER, new Character("butler", false)},
         {Names.COLONEL, new Character("colonel", false)},
+        {Names.COUNTESS, new Character("countess", false)},
         {Names.GANGSTER, new Character("gangster", false)},
         {Names.POLICE, new Character("policeman", false)},
-        {Names.BUTLER, new Character("butler", false)},
       };
 
       var characterKnowledgeBuilders = new List<KnowledgeBuilder>
       {
         // Actress
         new KnowledgeBuilder(entities, verbs)
-        {},
-        // Countess
+        {
+          //{"actress", "be", "murderer"},
+          /*{"actress", "be", "earl's daughter"},*/
+          {"actress", "study", "herbology"},
+          {"actress", "have", "scrap"},
+          {"scrap", "about", "nightshade"},
+          {"belongings", "contain", "nightshade"},
+          {"colonel", "own", "belongings"},
+          /*{"nightshade", "is", "fast-acting"},*/
+          /*{"nightshade", "look like", "blackcurrants"},*/
+          /*{"dining room", "contain", "blackcurrants"},*/
+          //{"nightshade", "poison", "earl"},
+          /*{"actress", "gets", "contents of will"},*/
+          {"earl", "fight", "war"},
+          /*{"earl", "friends with", "colonel"},*/ //Probably just a like relation
+          {"colonel", "fight", "war"},
+          {"earl", "marry", "countess"},
+          {"countess", "marry", "earl"},
+          /*{"barbital", "is", "sleeping aid"},*/
+          /*{"arsenic, "used as", "rat poison"},*/
+          {"earl", "employ", "butler"},
+        },
+        // Butler
         new KnowledgeBuilder(entities, verbs)
-        {},
+        {
+          /*{"nightshade", "look like", "blackcurrants"},*/
+          /*{"dining room", "contain", "blackcurrants"},*/
+          {"will", "on", "desk"},
+          {"study", "contain", "desk"},
+          {"study", "contain", "will"},
+          {"earl", "fight", "war"},
+          /*{"earl", "friends with", "colonel"},*/ //Probably just a like relation
+          {"colonel", "fight", "war"},
+          /*{"countess", "gets", "contents of will"},*/
+          {"colonel", "want", "promotion"},
+          {"earl", "marry", "countess"},
+          {"countess", "marry", "earl"},
+          /*{"countess", "wants to sell", "estate"},*/
+          {"book", "about", "barbital"},
+          {"study", "contain", "book"},
+          {"earl", "employ", "butler"},
+          // /*{"butler", "stealing from", "earl"},*/ // Not sure how to represent _and_ needs hiding/omitting in this graph
+          {"butler", "is", "nervous"},
+          /*{"butler", "allergic to", "plants"},*/
+          /*{"butler", "have", "rat poison"},*/
+          /*{"kitchen", "contain", "rat poison"},*/
+          {"earl", "owe", "gangster"},
+          {"earl", "meet", "gangster"},
+          {"gangster", "meet", "earl"},
+          /*{"gangster", "kill with", "arsenic"},*/
+          /*{"arsenic, "used as", "rat poison"},*/
+          /*{"master bedroom", "contain", "letter"},*/
+          {"gangster", "send", "letter"},
+          {"earl", "receive", "letter"}
+        },
         // Colonel
         new KnowledgeBuilder(entities, verbs)
-        {},
+        {
+          /*{"actress", "be", "earl's daughter"},*/
+          {"colonel", "own", "belongings"},
+          /*{"nightshade", "is", "fast-acting"},*/
+          /*{"dining room", "contain", "blackcurrants"},*/
+          /*{"countess", "gets", "contents of will"},*/
+          {"will", "on", "desk"},
+          {"study", "contain", "desk"},
+          {"study", "contain", "will"},
+          {"earl", "fight", "war"},
+          /*{"earl", "friends with", "colonel"},*/ //Probably just a like relation
+          {"colonel", "fight", "war"},
+          {"colonel", "resent", "earl"},
+          {"colonel", "want", "promotion"},
+          {"earl", "prevent", "promotion"},
+          {"earl", "marry", "countess"},
+          {"countess", "marry", "earl"},
+          {"earl", "employ", "butler"},
+          /*{"butler", "have", "rat poison"},*/
+           /*{"arsenic, "used as", "rat poison"},*/
+          {"earl", "owe", "gangster"}
+        },
+        // Countess
+        new KnowledgeBuilder(entities, verbs)
+        {
+          {"actress", "study", "herbology"},
+          /*{"dining room", "contain", "blackcurrants"},*/
+          /*{"countess", "gets", "contents of will"},*/
+          {"will", "on", "desk"},
+          {"study", "contain", "desk"},
+          {"study", "contain", "will"},
+          {"earl", "fight", "war"},
+          /*{"earl", "friends with", "colonel"},*/ //Probably just a like relation
+          {"colonel", "fight", "war"},
+          {"colonel", "resent", "earl"},
+          {"colonel", "want", "promotion"},
+          {"earl", "prevent", "promotion"},
+          {"note", "about", "promotion"},
+          {"earl", "marry", "countess"},
+          {"countess", "marry", "earl"},
+          /*{"countess", "wants to sell", "estate"},*/
+          {"earl", "use", "barbital"},
+          /*{"barbital", "is", "sleeping aid"},*/
+          /*{"earl", "have", "barbital tolerance"},*/
+          {"earl", "employ", "butler"},
+          {"butler", "is", "nervous"},
+          /*{"butler", "have", "rat poison"},*/
+          /*{"kitchen", "contain", "rat poison"},*/
+          {"earl", "meet", "gangster"},
+          {"gangster", "meet", "earl"},
+          /*{"arsenic, "used as", "rat poison"},*/
+          /*{"master bedroom", "contain", "letter"},*/
+          {"earl", "receive", "letter"}
+        },
         // Gangster
         new KnowledgeBuilder(entities, verbs)
-        {},
+        {
+          /*{"nightshade", "is", "fast-acting"},*/
+          /*{"dining room", "contain", "blackcurrants"},*/
+          /*{"earl", "friends with", "colonel"},*/ //Probably just a like relation
+          {"colonel", "fight", "war"},
+          {"earl", "marry", "countess"},
+          {"countess", "marry", "earl"},
+          /*{"butler", "stealing from", "earl"},*/
+          {"butler", "is", "nervous"},
+          {"earl", "owe", "gangster"},
+          {"earl", "meet", "gangster"},
+          {"gangster", "meet", "earl"},
+          /*{"gangster", "kill with", "arsenic"},*/
+          /*{"arsenic", "is", "slow-acting"},*/
+          /*{"arsenic, "used as", "rat poison"},*/
+          /*{"master bedroom", "contain", "letter"},*/
+          {"gangster", "send", "letter"},
+          {"earl", "receive", "letter"}
+        },
         // Police
-        new KnowledgeBuilder(entities, verbs)
-        {},
-        // Butler
         new KnowledgeBuilder(entities, verbs)
         {}
       };
