363c8adb42d93cf11bbcfab25a50bc7c283329d9
Adam Beddoe <adam.beddoe@gmail.com>
2019-04-11 20:35:16 +0100

    Refactored a bit more and added comments to explain the logic of methods.

--- WatsonAI/WatsonAI/src/Processes/PronounsProcess.cs
+++ WatsonAI/WatsonAI/src/Processes/PronounsProcess.cs
@@ -39,10 +39,30 @@ namespace WatsonAI
     /// <param name="tokens">A reference to a list of tokens to act on.</param>
     public void PreProcess(ref List<string> tokens)
     {
-      string entity = "";
-      var replacingItWord = CheckForItWord(tokens, out entity);
-      var inputCharacters = FindCharactersInInput(tokens);
+      var replacing = new List<Tuple<List<string>, List<string>>>();
 
+      replacing.AddRange(SimplePronounReplacements());
+      replacing.AddRange(ItPronounReplacements(tokens));
+      replacing.AddRange(CharacterPronounReplacements(tokens));
+
+      ReplaceWords(replacing, tokens);
+
+      //TODO: Else here is a good place to introduce the failstate.
+    }
+
+    /// <summary>
+    /// Returns all the simple patterns for a replacing pronouns in the tokens.
+    /// </summary>
+    /// <remarks>
+    /// I define simple to mean not needing to query the structure of the tokens: the replacement is
+    /// independent of the input tokens.
+    /// </remarks>
+    /// <param name="tokens">The tokens to check for nouns in.</param>
+    /// <returns>
+    /// List of tuples containing a list of tokens to match against, and list of tokens to replace.
+    /// </returns>
+    public List<Tuple<List<string>, List<string>>> SimplePronounReplacements()
+    {
       var replacing = new List<Tuple<List<string>, List<string>>>
       {
         Tuple.Create(new List<string> { "do", "you" }, new List<string> { "does", this.character.Name }),
@@ -59,11 +79,43 @@ namespace WatsonAI
         Tuple.Create(new List<string> { "my" }, new List<string> { "Watson", "'s" }),
         Tuple.Create(new List<string> { "mine" }, new List<string> { "Watson", "'s" })
       };
+      return replacing;
+    }
+
+    /// <summary>
+    /// Returns all the patterns for a replacing it-based pronouns in the tokens.
+    /// </summary>
+    /// <param name="tokens">The tokens to check for nouns in.</param>
+    /// <returns>
+    /// List of tuples containing a list of tokens to match against, and list of tokens to replace.
+    /// </returns>
+    public List<Tuple<List<string>, List<string>>> ItPronounReplacements(List<string> tokens)
+    {
+      var replacing = new List<Tuple<List<string>, List<string>>>();
+      
+      string entity = "";
+      var replacingItWord = CheckForItWord(tokens, out entity);
 
       if (replacingItWord)
       {
         replacing.Add(Tuple.Create(new List<string> { "it" }, new List<string> { "the", entity }));
       }
+      return replacing;
+    }
+
+    /// <summary>
+    /// Returns all the patterns for a replacing character-based pronouns in the tokens.
+    /// </summary>
+    /// <param name="tokens">The tokens to check for characters in.</param>
+    /// <returns>
+    /// List of tuples containing a list of tokens to match against, and list of tokens to replace.
+    /// </returns>
+    public List<Tuple<List<string>, List<string>>> CharacterPronounReplacements(List<string> tokens)
+    {
+      var replacing = new List<Tuple<List<string>, List<string>>>();
+
+      var inputCharacters = FindCharactersInInput(tokens);
+
       if (inputCharacters.Any())
       {
         var inputCharacter = inputCharacters.First();
@@ -73,8 +125,9 @@ namespace WatsonAI
           replacing.Add(Tuple.Create(new List<string> { "his" }, new List<string> { inputCharacter.Name, "'s" }));
           replacing.Add(Tuple.Create(new List<string> { "he" }, new List<string> { inputCharacter.Name }));
         }
-        if (inputCharacter.Gender == Gender.Female)
+        else if (inputCharacter.Gender == Gender.Female)
         {
+          // This needs to support her and hers
           replacing.Add(Tuple.Create(new List<string> { "her" }, new List<string> { inputCharacter.Name }));
           replacing.Add(Tuple.Create(new List<string> { "hers" }, new List<string> { inputCharacter.Name, "'s" }));
           replacing.Add(Tuple.Create(new List<string> { "she" }, new List<string> { inputCharacter.Name }));
@@ -92,9 +145,7 @@ namespace WatsonAI
           }
         }
       }
-      ReplaceWords(replacing, tokens);
-
-      //TODO: Else here is a good place to introduce the failstate.
+      return replacing;
     }
 
     /// <summary>
@@ -113,6 +164,14 @@ namespace WatsonAI
       ReplaceWords(replacing, tokens);
     }
 
+    /// <summary>
+    /// Applies a list of replacements to the list of tokens. 
+    /// </summary>
+    /// <param name="replacements">
+    /// A list of tuples, each of which has a list of tokens to match against, 
+    /// and a list of tokens to replace it with.
+    /// </param>
+    /// <param name="tokens"></param>
     private void ReplaceWords(List<Tuple<List<string>, List<string>>> replacements, List<string> tokens)
     {
       for (int i = 0; i < tokens.Count; i++)
@@ -134,9 +193,20 @@ namespace WatsonAI
       }
     }
 
+    /// <summary>
+    /// Checks whether two lists of strings are equal, ignoring case on each string.
+    /// </summary>
+    /// <param name="firstTokens">The first list of strings.</param>
+    /// <param name="secondTokens">The second list of strings.</param>
+    /// <returns>True if the two lists are equal, ignoring the case on each string.</returns>
     private bool AllWordsEqual(List<string> firstTokens, List<string> secondTokens)
      => firstTokens.Zip(secondTokens, (x, y) => x.Equals(y, StringComparison.OrdinalIgnoreCase)).All(x => x);
 
+    /// <summary>
+    /// Finds any character names in the list of tokens.
+    /// </summary>
+    /// <param name="tokens">The list of tokens to search.</param>
+    /// <returns>A list of character names in the tokens.</returns>
     private List<Character> FindCharactersInInput(List<string> tokens)
     {
       var storyCharacters = this.characters;
@@ -154,6 +224,12 @@ namespace WatsonAI
       return inputCharacters;
     }
 
+    /// <summary>
+    /// Checks for "it" token and finds the correct replacement in the sentence or in memory.
+    /// </summary>
+    /// <param name="tokens">The list of tokens to check for "it" word.</param>
+    /// <param name="word">Out parameter, the noun found to replace "it".</param>
+    /// <returns>True if a noun replacement for "it" was found.</returns>
     private bool CheckForItWord(List<string> tokens, out string word)
     {
       if (tokens.Contains("it"))
@@ -182,6 +258,12 @@ namespace WatsonAI
       return false;
     }
 
+    /// <summary>
+    /// Finds a noun that an it references.
+    /// </summary>
+    /// <param name="tokens">The tokens to search for noun.</param>
+    /// <param name="word">Out paramater, the noun found to replace "it".</param>
+    /// <returns>True if a noun was found.</returns>
     private bool FindItWord(IEnumerable<string> tokens, out string word)
     {
       Parse parse;
